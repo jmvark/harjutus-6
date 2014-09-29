e2f0d5bb62e6c7598240e05c19f57ece38d04176
JasonMax_2 <jason-max.vark@khk.ee>
2014-09-29 13:08:39 +0300

    tingimuslaused

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html;
+charset=utf-8">
+<title>harjutus-6</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<h2>Tingimuslaused</h2>
+	<?php
+	$a = 4;
+	$b = 5;
+	if ($a < $b) {
+		echo "4 on väiksem kui 5";
+	}
+
+	?>
+
+</body>
+</html>
